b57cacce0a301b58f3cd458c3b0d45786442f48e
yangkail <wewe6313@gmail.com>
2020-11-08 15:35:05 +0800

    1

---
+++ HomeWork1_6.java
@@ -0,0 +1,14 @@
+package HomeWork1;
+public class HomeWork1_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(5+5);  // 此數字為int5所以相加為10	
+		System.out.println(5+'5');	//此數字為int5+char'5'字元
+		// char為unicode16進位,轉換電腦10進位,對應為53,故相加為58
+		System.out.println(5+"5"); //此數字為int5+String"5"字串,故相加為55
+		
+	}
+
+}
